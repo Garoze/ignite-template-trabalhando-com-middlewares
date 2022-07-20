6baac5e82d00dd2c238e60360c8db8d58751aac2
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-07-20 16:23:12 -0300

    feat: All tests pass

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+   const { username } = request.headers;
+
+   const user = users.find((user) => user.username === username);
+   if (!user) {
+      return response.status(404).json({ error: 'Mensagem do erro' });
+   } 
+
+   request.user = user;
+   return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+   const { user } = request;
+   
+   if (user.pro === false) {
+      if (user.todos.length === 10)
+         return response.status(403).json({ error: 'Mensagem de error' });
+   } 
+   
+   return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+   const { username } = request.headers;
+   const { id } = request.params;
+
+   const user = users.find((user) => user.username === username);
+   if (!user) {
+      return response.status(404).json({ error: 'Mensagem do erro' });
+   } 
+
+   if (!validate(id)) {
+      return response.status(400).json({ error: 'Mensagem de erro' });
+   }
+
+   const todo = user.todos.find((todo) => todo.id === id);
+   if (!todo) {
+      return response.status(404).json({ error: 'Mensagem do erro' });
+   }
+
+   request.todo = todo;
+   request.user = user;
+
+   return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+   const { id } = request.params;
+   
+   const user = users.find((user) => user.id === id);
+   if (!user) {
+      return response.status(404).json({ error: 'Mensagem do erro' });
+   }
+
+   request.user = user; 
+
+   return next();
 }
 
 app.post('/users', (request, response) => {
